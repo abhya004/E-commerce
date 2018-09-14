3280186458c437e16fd87d66938a77253489c40a
Abhya <abhya.tripathi.at@gmail.com>
2018-09-14 17:41:44 +0530

    first

---
+++ js/my.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $('.bxslider').bxSlider({
+//       mode:'fade',
+//       captions: true,
+//     });
+//   });
+  $(document).ready(function(){
+  $('.bxslider').bxSlider({
+  mode: 'fade',
+  auto: true,
+  autoControls: true,
+  pause: 2000
+  });
+});
